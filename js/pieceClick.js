b7287656eadaa7dddfdb82dec26ea81341f5c7bf
Ashwanth A R <ashwanth1109@gmail.com>
2018-09-22 14:48:39 +0530

    Added piece click functionality to show that piece is selected

--- js/pieceClick.js
+++ js/pieceClick.js
@@ -5,5 +5,16 @@ const highlightNextMove = () => {
 const pieceClick = event => {
   console.log(`Piece was clicked`);
   console.log(event.currentTarget);
-  console.log($(event.currentTarget));
+  const $clickedPiece = $(event.currentTarget);
+  // console.log($(event.currentTarget).hasClass(`highlight`));
+  if ($clickedPiece.hasClass(`highlight`)) {
+    // SHOW THAT PIECE IS CLICKED
+    const $squares = $(`.highlight`);
+    for (let i = 0; i < $squares.length; i++) {
+      if ($squares.eq(i).hasClass(`selected`)) {
+        $squares.eq(i).removeClass(`selected`);
+      }
+    }
+    $clickedPiece.addClass(`selected`);
+  }
 };
