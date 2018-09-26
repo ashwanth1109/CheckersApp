e9d902da7d683d4808cc61f1ba05eeccb6db7c80
Ashwanth A R <ashwanth1109@gmail.com>
2018-09-26 14:13:10 +0530

    Some crucial minor fixes. Issue with new game functionality - it resets the board but it doesnt check for moves available

--- js/logic/startNewGame.js
+++ js/logic/startNewGame.js
@@ -1,3 +1,9 @@
 const startNewGame = () => {
   console.log(`starting a new game`);
+  $gameBoard.children().remove();
+  createBoard();
+  //------------------------------------------------------------------------------------
+  // Check for moves available for current player
+  //------------------------------------------------------------------------------------
+  checkForMovesAvailable();
 };
