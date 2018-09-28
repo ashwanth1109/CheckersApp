92752313f48cf918e159f4702a40f240b8b469da
Ashwanth A R <ashwanth1109@gmail.com>
2018-09-28 13:27:50 +0530

    Refactored create board

--- js/classes/square.js
+++ js/classes/square.js
@@ -1,44 +1,75 @@
-//-----SQUARE CLASS-----//
-
+//------------------------------------------------------------------------------------
+// Class to create square objects - takes in row, column and game size as params
+//------------------------------------------------------------------------------------
 class Square {
-  convertToValue(row, column) {
-    // console.log(typeof row);
-    if (row === 0 || row === 9 || column === 0 || column === 9) {
-      return null;
-    }
-    return (row - 1) * 8 + column - 1;
-  }
-
-  constructor(rowPosition, columnPosition) {
+  //------------------------------------------------------------------------------------
+  // Constructor function for class square
+  //------------------------------------------------------------------------------------
+  constructor(rowPosition, columnPosition, gameSize) {
+    //------------------------------------------------------------------------------------
+    // Store row and column as properties
+    //------------------------------------------------------------------------------------
     this.row = rowPosition;
     this.column = columnPosition;
 
+    //------------------------------------------------------------------------------------
+    // Based on row and column, determine the row and column positions of all diagonals
+    //------------------------------------------------------------------------------------
     this.topLeftArray = [rowPosition - 1, columnPosition - 1];
+    this.topRightArray = [rowPosition - 1, columnPosition + 1];
+    this.botLeftArray = [rowPosition + 1, columnPosition - 1];
+    this.botRightArray = [rowPosition + 1, columnPosition + 1];
+
+    //------------------------------------------------------------------------------------
+    // Using the diagonal arrays above, we convert each of these to correspond to a div
+    // in the squares array. To access that we need its value position
+    //------------------------------------------------------------------------------------
     this.topLeft = this.convertToValue(
       this.topLeftArray[0],
-      this.topLeftArray[1]
+      this.topLeftArray[1],
+      gameSize
     );
 
-    this.topRightArray = [rowPosition - 1, columnPosition + 1];
     this.topRight = this.convertToValue(
       this.topRightArray[0],
-      this.topRightArray[1]
+      this.topRightArray[1],
+      gameSize
     );
 
-    this.botLeftArray = [rowPosition + 1, columnPosition - 1];
     this.botLeft = this.convertToValue(
       this.botLeftArray[0],
-      this.botLeftArray[1]
+      this.botLeftArray[1],
+      gameSize
     );
 
-    this.botRightArray = [rowPosition + 1, columnPosition + 1];
     this.botRight = this.convertToValue(
       this.botRightArray[0],
-      this.botRightArray[1]
+      this.botRightArray[1],
+      gameSize
     );
 
-    this.value = this.convertToValue(this.row, this.column);
+    this.value = this.convertToValue(this.row, this.column, gameSize);
 
+    //------------------------------------------------------------------------------------
+    // Keeps track of whether the square has a piece or not
+    //------------------------------------------------------------------------------------
     this.hasPiece = false;
+  }
+
+  //------------------------------------------------------------------------------------
+  // Method to that takes in row, column and game size as params and returns null
+  // if the square is outside the bounds of the board and returns value to help identify
+  // square if it is inside the board
+  //------------------------------------------------------------------------------------
+  convertToValue(row, column, gameSize) {
+    if (
+      row === 0 ||
+      row === gameSize + 1 ||
+      column === 0 ||
+      column === gameSize + 1
+    ) {
+      return null;
+    }
+    return (row - 1) * gameSize + column - 1;
   }
 }
--- js/interface/createBoard.js
+++ js/interface/createBoard.js
@@ -1,37 +1,65 @@
+//------------------------------------------------------------------------------------
+// Function that creates the game board
+//------------------------------------------------------------------------------------
 const createBoard = () => {
-  // console.log(`creating board`);
-  //-----FOR LOOP TO CREATE ROWS-----//
+  //------------------------------------------------------------------------------------
+  // For Loop to create 'gameSize' number of rows
+  //------------------------------------------------------------------------------------
   for (let i = 1; i <= gameSize; i++) {
-    //-----ROWS HAVE CLASS OF ROW: APPEND TO GAME BOARD-----//
+    //------------------------------------------------------------------------------------
+    // Create divs with class row and append them to the game board
+    //------------------------------------------------------------------------------------
     const $row = $(`<div class="row">`).appendTo($gameBoard);
-    //-----FOR LOOP TO CREATE SQUARES-----//
+    //------------------------------------------------------------------------------------
+    // For Loop to create 'gameSize' number of squares
+    //------------------------------------------------------------------------------------
     for (let j = 1; j <= gameSize; j++) {
-      //-----SQUARES HAVE CLASS OF SQUARE: APPEND TO ROW-----//
+      //------------------------------------------------------------------------------------
+      // Create divs with class square and append them to the row divs
+      //------------------------------------------------------------------------------------
       const $square = $(`<div class="square">`).appendTo($row);
+      //------------------------------------------------------------------------------------
+      // Adds a horizontal border between each squares
+      //------------------------------------------------------------------------------------
       $(`<div class="horizontal-border">`).appendTo($row);
-      //-----data STORES SQUARE OBJECT CREATED FROM SQUARE CLASS-----//
-      const data = new Square(i, j);
+      //------------------------------------------------------------------------------------
+      // We create square objects from the square class and add them as data attributes
+      //------------------------------------------------------------------------------------
+      const data = new Square(i, j, gameSize);
       $square.data(`data`, data);
-      //
-      //-----ADD CLASS DARK TO ALTERNATING SQUARES-----//
+      //------------------------------------------------------------------------------------
+      // Identify alternating squares
+      //------------------------------------------------------------------------------------
       if (!((i + j) % 2)) {
+        //------------------------------------------------------------------------------------
+        // Add class dark to alternating squares
+        //------------------------------------------------------------------------------------
         $square.addClass(`dark`);
-        //-----ADD PLAYER 2 PIECES ON TO THE BOARD-----//
+        //------------------------------------------------------------------------------------
+        // Add player pieces at their starting positions on dark square
+        //------------------------------------------------------------------------------------
         if (i < 4) {
-          //-----ADD PLAYER 2 PIECES AND SET CLICK HANDLER-----//
+          //------------------------------------------------------------------------------------
+          // Add player 2 pieces and set checker data object on the checker pieces
+          //------------------------------------------------------------------------------------
           addCheckers($square, 2);
-        }
-        //-----ADD PLAYER 1 PIECES ON TO THE BOARD-----//
-        else if (i > 5) {
-          //-----ADD PLAYER 1 PIECES AND SET CLICK HANDLER-----//
+        } else if (i > 5) {
+          //------------------------------------------------------------------------------------
+          // Add player 2 pieces and set checker data object on the checker pieces
+          //------------------------------------------------------------------------------------
           addCheckers($square, 1);
         }
       }
-      //-----ADD CLASS LIGHT TO ALTERNATING SQUARES-----//
+      //------------------------------------------------------------------------------------
+      // Add class light to every other alternating square
+      //------------------------------------------------------------------------------------
       else {
         $square.addClass(`light`);
       }
     }
+    //------------------------------------------------------------------------------------
+    // Adds a vertical border between rows on the game board
+    //------------------------------------------------------------------------------------
     $(`<div class="vertical-border">`).appendTo($gameBoard);
   }
 };
